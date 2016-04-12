d82a25151172f2a710fdd39dec6f97811da92b8e
galarodo <Arion@Silver-Streak.local>
2016-04-11 21:24:42 -0400

    Added code to Beep when the user clicks
    Uses PyAudio

--- macLogger.py
+++ macLogger.py
@@ -1,13 +1,47 @@
 from pynput.mouse import Listener
+import pyaudio
+import wave
+
+
+CHUNK = 1024
+
+def Beep():
+    wf = wave.open('beep.wav', 'rb')
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+    # read data
+    data = wf.readframes(CHUNK)
+
+    # play stream (3)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    # stop stream (4)
+    stream.stop_stream()
+    stream.close()
+
+    # close PyAudio (5)
+    p.terminate()
 
 
 def on_click(x, y, button, pressed):
+    if x == 0 and y == 0:
+        Beep()
+        return False
     print('{0} at {1}'.format(
         'Pressed' if pressed else 'Released',
         (x, y)))
-    if not pressed:
-        # Stop listener
-        return False
+    # if not pressed:
+    #     # Stop listener
+    #     return False
+    if pressed:
+        Beep()
 
 
 # Collect events until released
