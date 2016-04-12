6c23ceeaec7d4fd9d4a48eb7dfe558b228e71c28
galarodo <Arion@Silver-Streak.local>
2016-04-11 20:40:07 -0400

    Added file for logging on Mac
    Using Pynput to record mouse and keyboard on OS X.  Should also work on
    windows

---
+++ macLogger.py
@@ -0,0 +1,15 @@
+from pynput.mouse import Listener
+
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if not pressed:
+        # Stop listener
+        return False
+
+
+# Collect events until released
+with Listener(on_click=on_click) as listener:
+    listener.join()
