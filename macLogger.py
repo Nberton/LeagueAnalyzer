3519a5bef213da5114b0f5ee8507ad8390048d8b
galarodo <Arion@Silver-Streak.local>
2016-04-14 23:09:59 -0400

    Beep twice on end
    Now beeps twice on end instead of once.  Easier to differentiate when
    it begins and ends

--- macLogger.py
+++ macLogger.py
@@ -52,6 +52,7 @@ def on_click(x, y, button, pressed):
 
     if x == 0 and y == 0 and button == mouse.Button.left:
         Beep()
+        Beep()
         keyboardListener.stop()
         return False
 
