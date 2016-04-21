c13d62f7eb59aa9d22e47f27509a842843597723
Nathan Berton <nathan.spero.berton@gmail.com>
2016-04-21 13:51:28 -0400

    Start of Visualization coalation

---
+++ visualization/paralellconverter.py
@@ -0,0 +1,34 @@
+
+
+#creates a dictionary of the stats
+def gameAnalyzer(inStream):
+	#get champ name from user
+	
+	#get champ lane from user
+
+	#read that file
+
+
+def main():
+	print "Paralell Coordinates constructor for keys"
+	#create header row
+	outfileN = raw_input("Please enter a file name: ")
+	of = open(outfileN,'w')
+	#enter input loop
+	count = 0
+	inF = raw_input("please input a datafile Name")
+	while(inF != "end" or inF != "q"):
+		count++
+		#check for valid file
+
+		#if failed go around 
+
+		#else analyze the game and throw it in the data file
+
+	#end program with some checking output
+	print "Wrote " + str(count) + " Games of data"
+	print "Closing File Writer"
+
+
+if __name__ == '__main__':
+	main()
