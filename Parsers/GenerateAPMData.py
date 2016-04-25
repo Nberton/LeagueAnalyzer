88361f09f11d90fabcc9555cfdb168a036ca49db
galarodo <Arion@rpi-wl-1057.dynamic.rpi.edu>
2016-04-25 17:00:16 -0400

    Created a script that parses Actions per minute of every game in the Data folder

---
+++ Parsers/GenerateAPMData.py
@@ -0,0 +1,50 @@
+import os
+
+
+def CollectAPM(folder):
+    mouse = open(folder + '/mouselogs.txt', 'r')
+    keyboard = open(folder + '/keylogs.txt', 'r')
+    output = open(folder + '/Parsed Data/actionsPerMinute.csv', 'w')
+
+    actions = {}
+
+    for line in mouse:
+        line.replace(' ', '')
+        newLine = line.strip().split('-')
+
+        time = int(float(newLine[0])) / 60
+        if time in actions:
+            actions[time] += 1
+        else:
+            actions[time] = 1
+
+    for line in keyboard:
+        line.replace(' ', '')
+        newLine = line.strip().split('-')
+
+        time = int(float(newLine[0])) / 60
+        # print time
+        if time in actions:
+            actions[time] += 1
+        else:
+            actions[time] = 1
+
+    output.write("time,actions")
+    for item in actions:
+        output.write("\n%d,%d" % (item, actions[item]))
+        # print item, ":", actions[item]
+
+    output.close()
+    mouse.close()
+    keyboard.close()
+
+
+if __name__ == '__main__':
+    rootDir = '../Data'
+    os.chdir(rootDir)
+
+    for gameFolder in os.listdir('.'):
+        print gameFolder
+        CollectAPM(gameFolder)
+
+
