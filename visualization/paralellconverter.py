db2691b5c9bfa4988f0923ebd6267f88bfea6405
Nathan Berton <nathan.spero.berton@gmail.com>
2016-04-28 17:11:38 -0400

    Para Coords for user test

--- visualization/paralellconverter.py
+++ visualization/paralellconverter.py
@@ -2,11 +2,16 @@ import os.path
 
 
 #creates a dictionary of the stats
-def gameAnalyzer(inFile):
+def gameAnalyzer(inFileName):
+	inFile = open(inFileName + '/keylogs.txt')
+	infoFile = open(inFileName + '/README.txt')
+	info = list()
+	for line in infoFile:
+		info.append(line)
 	#get champ name from user
-	champ = raw_input("Please input Champion Name: ")
+	champ = info[0].rstrip()
 	#get champ lane from user
-	lane = raw_input("Please input Lane: ")
+	lane = info[1].rstrip()
 	#read that file
 	countMap = dict()
 	for line in inFile:
@@ -21,31 +26,32 @@ def gameAnalyzer(inFile):
 		+ str(countMap['w']) + ','  + str(countMap['e']) + ','+ str(countMap['r']) + ',' 
 		+ str(countMap['d']) + ',' + str(countMap['f']))
 	print outLine
+	inFile.close()
+	infoFile.close()
 	return outLine
 
 
 def main():
-	print "Paralell Coordinates constructor for keys"
+	print "Parallel Coordinates constructor for keys"
 	#create header row
-	outfileN = raw_input("Please enter a file name: ")
+	outfileN = raw_input("Please enter an output file name: ")
 	outfileN += '.csv'
 	of = open(outfileN,'w')
-	outLine = "Champion,Lane,Q,W,E,R,D,F\n"
+	outLine = "Champion,Lane,Q,W,E,R,D,F"
 	of.write(outLine)
 	#enter input loop
 	count = 0
-	inFN = raw_input("please input a datafile Name: ")
+	inFN = raw_input("please input a data folder Name: ")
 	while(inFN != 'end' and inFN != 'q'):
 		print inFN
 		#check for valid file
-		if not os.path.isfile(inFN):	#if failed go around
+		if not os.path.exists(inFN):	#if failed go around
 			print "That is not a valid file! Please try again or q to exit"
 			inFN = raw_input("please input a datafile Name or q:")
 			continue
 		count += 1
 	 	#else analyze the game and throw it in the data file
-	 	inF = open(inFN)
-	 	outLine = gameAnalyzer(inF)
+	 	outLine = gameAnalyzer(inFN)
 	 	of.write('\n'+outLine)
 	 	inFN = raw_input("please input a datafile Nam or q: ")
 	#end program with some checking output
