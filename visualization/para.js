ca08f19828a12531495dbc0c7cf5d8b7aecfc380
Nathan Berton <nathan.spero.berton@gmail.com>
2016-05-02 18:43:33 -0400

    Colored and fixed a few paracoord bugs

--- visualization/para.js
+++ visualization/para.js
@@ -1,11 +1,30 @@
+
+var colors = d3.scale.category20b();
+
 d3.csv('test.csv', function(data) {
+
+	   //color on champ type
+  var colorgen = d3.scale.ordinal()
+    .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
+            "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
+            "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
+
+  var color = function(d) { return colorgen(d.Champion); };
 
   var parcoords = d3.parcoords()("#para")
     .data(data)
+    .alpha(0.6) 
+    .color(color)
+    .alphaOnBrushed(0.15)
     .render()
-    .createAxes() 
-    .alpha(0.35)
+    .createAxes()
+    .reorderable()
     .brushMode("1D-axes")  // enable brushing
-  	.reorderable()
+
+
+
+  d3.selectAll(".parcoords .label").attr("y", -5); //make title sit a little 
+  	    // .shadows()
+
 
 });
